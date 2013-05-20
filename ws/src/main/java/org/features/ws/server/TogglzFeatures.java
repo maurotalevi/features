0e11e6b128399241138590519340602cbc087e7f
Mauro Talevi <mauro.talevi@aquilonia.org>
2013-05-20 11:53:35 +0200

    Simplified endpoint implementation

--- ws/src/main/java/org/features/ws/server/TogglzFeatures.java
+++ ws/src/main/java/org/features/ws/server/TogglzFeatures.java
@@ -1,42 +1,26 @@
 package org.features.ws.server;
 
-import java.util.NoSuchElementException;
-
 import javax.jws.WebService;
 
-import org.togglz.core.Feature;
 import org.togglz.core.context.FeatureContext;
 import org.togglz.core.manager.FeatureManager;
+import org.togglz.core.util.FeatureMap;
 
 @WebService(endpointInterface = "org.features.ws.server.Features", serviceName = "Features")
 public class TogglzFeatures implements Features {
     
-    private FeatureManager manager;
+    private FeatureMap featureMap;
     
     public TogglzFeatures(){
         this(FeatureContext.getFeatureManager());
     }
     
 	public TogglzFeatures(FeatureManager manager) {
-        this.manager = manager;
+        featureMap = new FeatureMap(manager);
     }
 
     public boolean isActive(String name) {
-		try {
-			Feature feature = feature(manager, name);
-			return manager.isActive(feature);
-		} catch (NoSuchElementException e) {
-			return false;
-		}
-	}
-
-	private Feature feature(FeatureManager featureManager, String name) {
-		for (Feature feature : featureManager.getFeatures()) {
-			if (name.equals(feature.name())) {
-				return feature;
-			}
-		}
-		throw new NoSuchElementException(name);
+        return featureMap.get(name);
 	}
 
 }
